0a230d82bfd66ba198bea7bcd4d3787cd42955ae
Richard Folea <Folea3@gmail.com>
2023-08-10 12:41:21 -0400

    Update json_locale_translator.py

--- json_locale_translator.py
+++ json_locale_translator.py
@@ -4,6 +4,7 @@ import copy
 import os
 import argparse
 from python_translator import Translator
+from pathlib import Path
 
 # possible languages [language, code]:
 lang_opt_list = [["Afrikaans","af"],["Albanian","sq"],["Amharic","am"],["Arabic","ar"],["Armenian","hy"],["Assamese","as"],["Aymara","ay"],["Azerbaijani","az"],["Bambara","bm"],["Basque","eu"],["Belarusian","be"],["Bengali","bn"],["Bhojpuri","bho"],["Bosnian","bs"],["Bulgarian","bg"],["Catalan","ca"],["Cebuano","ceb"],["Chinese (Simplified)","zh"],["Chinese (Traditional)","zh-TW"],["Corsican","co"],["Croatian","hr"],["Czech","cs"],["Danish","da"],["Dhivehi","dv"],["Dogri","doi"],["Dutch","nl"],["English","en"],["Esperanto","eo"],["Estonian","et"],["Ewe","ee"],["Filipino (Tagalog)","fil"],["Finnish","fi"],["French","fr"],["Frisian","fy"],["Galician","gl"],["Georgian","ka"],["German","de"],["Greek","el"],["Guarani","gn"],["Gujarati","gu"],["Haitian Creole","ht"],["Hausa","ha"],["Hawaiian","haw"],["Hebrew","he"],["Hindi","hi"],["Hmong","hmn"],["Hungarian","hu"],["Icelandic","is"],["Igbo","ig"],["Ilocano","ilo"],["Indonesian","id"],["Irish","ga"],["Italian","it"],["Japanese","ja"],["Javanese","jv"],["Kannada","kn"],["Kazakh","kk"],["Khmer","km"],["Kinyarwanda","rw"],["Konkani","gom"],["Korean","ko"],["Krio","kri"],["Kurdish","ku"],["Kurdish (Sorani)","ckb"],["Kyrgyz","ky"],["Lao","lo"],["Latin","la"],["Latvian","lv"],["Lingala","ln"],["Lithuanian","lt"],["Luganda","lg"],["Luxembourgish","lb"],["Macedonian","mk"],["Maithili","mai"],["Malagasy","mg"],["Malay","ms"],["Malayalam","ml"],["Maltese","mt"],["Maori","mi"],["Marathi","mr"],["Meiteilon (Manipuri)","mni-Mtei"],["Mizo","lus"],["Mongolian","mn"],["Myanmar (Burmese)","my"],["Nepali","ne"],["Norwegian","no"],["Nyanja (Chichewa)","ny"],["Odia (Oriya)","or"],["Oromo","om"],["Pashto","ps"],["Persian","fa"],["Polish","pl"],["Portuguese (Portugal, Brazil)","pt"],["Punjabi","pa"],["Quechua","qu"],["Romanian","ro"],["Russian","ru"],["Samoan","sm"],["Sanskrit","sa"],["Scots Gaelic","gd"],["Sepedi","nso"],["Serbian","sr"],["Sesotho","st"],["Shona","sn"],["Sindhi","sd"],["Sinhala (Sinhalese)","si"],["Slovak","sk"],["Slovenian","sl"],["Somali","so"],["Spanish","es"],["Sundanese","su"],["Swahili","sw"],["Swedish","sv"],["Tagalog (Filipino)","tl"],["Tajik","tg"],["Tamil","ta"],["Tatar","tt"],["Telugu","te"],["Thai","th"],["Tigrinya","ti"],["Tsonga","ts"],["Turkish","tr"],["Turkmen","tk"],["Twi (Akan)","ak"],["Ukrainian","uk"],["Urdu","ur"],["Uyghur","ug"],["Uzbek","uz"],["Vietnamese","vi"],["Welsh","cy"],["Xhosa","xh"],["Yiddish","yi"],["Yoruba","yo"],["Zulu","zu"]]
@@ -20,7 +21,7 @@ parser.add_argument('-d', '--dest', help='Destination folder for the translated 
 parser.add_argument('-l', '--langs', nargs='+', help='List of languages to translate to, by language code. Example: -l hi ar es fr pt ja')
 parser.add_argument('-F', '--force', action='store_true', help='Force update all translations (even if they already exist). Default: False')
 parser.add_argument('-s', '--sort', action='store_true', help='Sort the base language keys alphabetically. Default: False')
-parser.add_argument('-L', '--list', action='store_true', help='List all available languages (or any from google translate that are missing)')
+parser.add_argument('-L', '--list', action='store_true', help='List all available languages')
 
 args = parser.parse_args()
 
@@ -63,7 +64,7 @@ else:
 
 
 # output relative path
-dest_path = 'locales'
+DEST_PATH = Path('./locales')
 
 # source code
 source_language_code = args.base or 'en'
@@ -85,7 +86,7 @@ if SORT_BASE_LANGUAGE_KEYS:
     language_list.append([source_language, source_language_code])
 
 # load source file
-jsondata = json.load(open(dest_path + '/' + source_file))
+jsondata = json.load(open(DEST_PATH / source_file, encoding='utf-8'))
 translator = Translator()
 
 def countKeysInObject(obj):
@@ -138,15 +139,15 @@ def main():
         language_code = language_list[i][1]
 
         jsonlangdata = {}
-        if language_code + '.json' in os.listdir(dest_path):
-            jsonlangdata = json.load(open(dest_path + '/' + language_code + '.json'))
+        if language_code + '.json' in os.listdir(DEST_PATH):
+            jsonlangdata = json.load(open(DEST_PATH / f'{language_code}.json', encoding='utf-8'))
         
         tmpdata = copy.deepcopy(jsondata)
         tmplangdata = copy.deepcopy(jsonlangdata)
 
         newdata = tx(tmpdata, tmplangdata, language)
 
-        newfile = open(dest_path + '/' + language_code + '.json', 'w')
+        newfile = open(DEST_PATH / f'{language_code}.json', 'w', encoding='utf-8')
         newfile.write(json.dumps(newdata, indent=4, sort_keys=True, ensure_ascii=False))
         newfile.close()
 
